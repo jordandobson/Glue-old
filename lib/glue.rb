c87efbd25b1f69986f0df50a83bfa4a72685d8fa
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-06-24 17:23:23 -0700

    First Version of Glue Gem - Happy Birthday

---
+++ lib/glue.rb
@@ -0,0 +1,84 @@
+require 'rubygems'
+require 'httparty'
+require 'nokogiri'
+require 'open-uri'
+
+module Glue
+
+  VERSION = '0.0.1'
+  
+  class AuthError < StandardError; end
+
+  class Client
+
+    DOMAIN = 'gluenow.com'
+    POST   = '/api/post'
+    USER   = '/api/user'
+
+    include  HTTParty
+
+    def initialize site, user, pass
+      raise  AuthError, 'Username, Password or Account is blank.' if site.empty? || user.empty? || pass.empty?
+      @user               = user
+      @pass               = pass
+      @site               = "#{site}.#{DOMAIN}"
+      self.class.base_uri   @site
+    end
+
+    def valid_site?
+      Nokogiri::HTML( open("http://#{@site}")).at('body#login') ? true : false
+    end
+    
+    def user_info
+      self.class.post( USER, :query => {})    
+      # RETURNS      
+      #       { "rsp"     => { 
+      #         "user"    => { 
+      #         "author"  => "Jordan",
+      #         "email"   => "jordandobson@gmail.com",
+      #         "admin"   => "true" }, 
+      #         "stat"    => "ok" }}
+    end
+
+    def post title, body, *opts
+      self.class.post( 
+        POST,
+        :query  =>  {
+        :title  =>  title, 
+        :body   =>  body, 
+        :draft  =>  opts.include?(:draft), 
+        :author =>  opts.include?(:author)
+      })
+      
+      # RETURNS
+      #       { "rsp"     => {
+      #         "post"    => {
+      #         "title"   => "$100 Box Spring",
+      #         "url"     => "http://bit.ly/pBGWf",
+      #         "id"      => "12415", 
+      #         "longurl" => "http://sqd.gluenow.com/feeds/1/posts/12415/" },
+      #         "stat"    => "ok" }}
+    end
+
+  end
+
+end
+
+# <rsp stat="ok">
+#   <post>
+#     <id>12415</id>
+#     <url>http://bit.ly/pBGWf</url>
+#     <longurl>http://sqd.gluenow.com/feeds/1/posts/12415/</longurl>
+#     <title>$100 Box Spring</title>
+#   </post>
+# </rsp>
+# 
+# <rsp stat="ok">
+#   <user>
+#     <author>Jordan</author>
+#     <email>jordandobson@gmail.com</email>
+#     <admin>true</admin>
+#   </user>
+# </rsp>
+
+	
---
+++ test/test_glue.rb
@@ -0,0 +1,74 @@
+require "test/unit"
+require "glue"
+
+class TestGlue < Test::Unit::TestCase
+
+  def setup
+    @a      = 'sqd'
+    @u      = 'username'
+    @p      = 'password'
+    @c      = Glue::Client.new( @a, @u, @p )
+    @title  = "My Title"
+    @body   = "Body Text"
+  end
+  
+  def test_raises_without_account_url
+    assert_raise Glue::AuthError do
+      Glue::Client.new( '', @u, @p )
+    end
+  end
+  
+  def test_raises_without_user
+    assert_raise Glue::AuthError do
+      Glue::Client.new( @a, '', @p )
+    end
+  end
+  
+  def test_raises_without_password
+    assert_raise Glue::AuthError do
+      Glue::Client.new( @a, @u, '' )
+    end
+  end
+  
+  def test_raises_with_number_account_url
+    assert_raise NoMethodError do
+      Glue::Client.new( 00, @u, @p )
+    end
+  end
+  
+  def test_raises_with_number_user
+    assert_raise NoMethodError do
+      Glue::Client.new( @a, 00, @p )
+    end
+  end
+  
+  def test_raises_with_number_password
+    assert_raise NoMethodError do
+      Glue::Client.new( @a, @u, 00 )
+    end
+  end
+  
+  def test_site_is_valid
+    actual    = Glue::Client.new( @a, @u, @p ).valid_site?
+    expected  = true
+    assert_equal expected, actual
+  end
+
+  def test_site_is_invalid
+    actual    = Glue::Client.new( "x", @u, @p ).valid_site?
+    expected  = false
+    assert_equal expected, actual
+  end
+  
+  def test_site_is_invalid
+    actual    = Glue::Client.new( "x", @u, @p ).valid_site?
+    expected  = false
+    assert_equal expected, actual
+  end
+  
+#   def test_options_are_accepted
+#     expected = [:author, :draft]
+#     assert_equal expected, @c.post(@title, @body, :author, :draft)
+#   end
+
+end
