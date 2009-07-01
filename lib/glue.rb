ef8c6e2c1ffba998a2ba665a4f4ea5f32621357f
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-07-01 00:06:43 -0700

    Stubbed out tests

--- lib/glue.rb
+++ lib/glue.rb
@@ -6,7 +6,7 @@ require 'open-uri'
 module Glue
 
   VERSION = '0.0.1'
-  
+
   class AuthError < StandardError; end
 
   class Client
@@ -14,71 +14,39 @@ module Glue
     DOMAIN = 'gluenow.com'
     POST   = '/api/post'
     USER   = '/api/user'
+    NEWS   = '/news/rss'
 
     include  HTTParty
     format  :xml
 
     def initialize site, user, pass
-      raise  AuthError, 'Username, Password or Account is blank.' if site.empty? || user.empty? || pass.empty?
-      
+      raise  AuthError, 'Username, Password or Account is blank.' \
+        if site.empty? || user.empty? || pass.empty?
       @auth   = { :username => user, :password => pass }
       @site   = "#{site}.#{DOMAIN}"
-      
       self.class.base_uri @site
     end
 
     def valid_site?
       Nokogiri::HTML(open( "http://#{@site}" )).at( 'body#login' ) ? true : false
     end
-    
+
     def user_info
-      self.class.post( USER, :query => {}, :basic_auth => @auth)    
-      # RETURNS      
-      #       { "rsp"     => { 
-      #         "user"    => { 
-      #         "author"  => "Jordan",
-      #         "email"   => "jordandobson@gmail.com",
-      #         "admin"   => "true" }, 
-      #         "stat"    => "ok" }}
+      self.class.post( USER, :query => {}, :basic_auth => @auth)
     end
 
     def post title, body, *opts
-      self.class.post( 
+      self.class.post(
         POST,
-        :query  =>  {
-        :title  =>  title, 
-        :body   =>  body, 
-        :draft  =>  opts.include?( :draft  ), 
-        :author =>  opts.include?( :author )},
-        :basic_auth => @auth )
-      # RETURNS
-      #       { "rsp"     => {
-      #         "post"    => {
-      #         "title"   => "$100 Box Spring",
-      #         "url"     => "http://bit.ly/pBGWf",
-      #         "id"      => "12415", 
-      #         "longurl" => "http://sqd.gluenow.com/feeds/1/posts/12415/" },
-      #         "stat"    => "ok" }}
+        :query      =>  {
+        :title      =>  title,
+        :body       =>  body,
+        :draft      =>  opts.include?( :draft  ),
+        :author     =>  opts.include?( :author )},
+        :basic_auth =>  @auth
+      )
     end
 
   end
 
 end
-
-# For Reference This is the success responses in XML
-# <rsp stat="ok">
-#   <post>
-#     <id>12415</id>
-#     <url>http://bit.ly/pBGWf</url>
-#     <longurl>http://sqd.gluenow.com/feeds/1/posts/12415/</longurl>
-#     <title>$100 Box Spring</title>
-#   </post>
-# </rsp>
-# 
-# <rsp stat="ok">
-#   <user>
-#     <author>Jordan</author>
-#     <email>jordandobson@gmail.com</email>
-#     <admin>true</admin>
-#   </user>
-# </rsp>
--- test/test_glue.rb
+++ test/test_glue.rb
@@ -1,71 +1,119 @@
 require "test/unit"
 require "glue"
 require "mocha"
+require "fakeweb"
+
+# Makes sure this doesn't hit the network
+FakeWeb.allow_net_connect = false
 
 class TestGlue < Test::Unit::TestCase
 
-  # TO DO
-  # - Stub Network Tests
-  # - Test user_info method
-  # - Test post method
-
   def setup
-    @a      = 'sqd'
-    @u      = 'username'
-    @p      = 'password'
-    @c      = Glue::Client.new( @a, @u, @p )
-    @title  = "My Title"
-    @body   = "Body Text"
+    @a          = 'sqd'
+    @u          = 'username'
+    @p          = 'password'
+    @c          = Glue::Client.new( @a, @u, @p )
+    @title      = "My Title"
+    @body       = "Body Text"
+    @url        = "http://bit.ly/sakIM"
+    @id         = "14415"
+    @lurl       = "http://jordandobson.com"
+    @resp_fail  = {}
+    
+    @resp_ok    = { "rsp"     => {
+                    "user"    => {
+                    "author"  => "Jordan",
+                    "admin"   => "true"  ,
+                    "email"   => nil    },
+                    "stat"    => "ok"   }}
+                    
+    @post_ok    = { "rsp"     => {
+                    "post"    => {
+                    "title"   => @title , 
+                    "url"     => @url   , 
+                    "id"      => @id    , 
+                    "longurl" => @lurl  },
+                    "stat"    => "ok"   }}
+
   end
-  
+
   def test_raises_without_account_url
     assert_raise Glue::AuthError do
       Glue::Client.new( '', @u, @p )
     end
   end
-  
+
   def test_raises_without_user
     assert_raise Glue::AuthError do
       Glue::Client.new( @a, '', @p )
     end
   end
-  
+
   def test_raises_without_password
     assert_raise Glue::AuthError do
       Glue::Client.new( @a, @u, '' )
     end
   end
-  
+
   def test_raises_with_number_account_url
     assert_raise NoMethodError do
       Glue::Client.new( 00, @u, @p )
     end
   end
-  
+
   def test_raises_with_number_user
     assert_raise NoMethodError do
       Glue::Client.new( @a, 00, @p )
     end
   end
-  
+
   def test_raises_with_number_password
     assert_raise NoMethodError do
       Glue::Client.new( @a, @u, 00 )
     end
   end
-  
+
   def test_site_is_valid
+    OpenURI.stubs(:open_uri).returns('<body id="login"></body>')
     assert @c.valid_site?
   end
 
   def test_site_is_invalid
-    c = Glue::Client.new("nogood", @u, @p)
-    assert !c.valid_site?
+    OpenURI.stubs(:open_uri).returns('<body></body>')
+    assert   !@c.valid_site?
   end
 
-#   def test_options_are_accepted
-#     expected = [:author, :draft]
-#     assert_equal expected, @c.post(@title, @body, :author, :draft)
-#   end
+  def test_user_info_valid
+    Glue::Client.stubs(:post).returns(@resp_ok)
+    actual       = @c.user_info
+    assert_equal   "ok",     actual["rsp"]["stat"]
+    assert                   actual["rsp"]["user"]
+    assert_equal   "Jordan", actual["rsp"]["user"]["author"]
+    assert_equal   "true",   actual["rsp"]["user"]["admin"]
+    assert_equal   nil,      actual["rsp"]["user"]["email"]
+  end
+
+  def test_user_info_invalid
+    Glue::Client.stubs(:post).returns(@resp_fail)
+    actual       = @c.user_info
+    assert_equal   @resp_fail, actual
+  end
+
+  def test_bad_post_response
+    Glue::Client.stubs(:post).returns(@resp_fail)
+    actual       = @c.post(@title, @body)
+    assert_equal   @resp_fail, actual
+  end
+
+  def test_good_post_response
+    Glue::Client.stubs(:post).returns(@post_ok)
+    actual       = @c.post(@title, @body)
+    assert_equal   "ok",     actual["rsp"]["stat"]
+    assert                   actual["rsp"]["post"]
+    assert_equal   @title,   actual["rsp"]["post"]["title"]
+    assert_equal   @url,     actual["rsp"]["post"]["url"]
+    assert_equal   @id,      actual["rsp"]["post"]["id"]
+    assert_equal   @lurl,    actual["rsp"]["post"]["longurl"]
+  end
 
 end
